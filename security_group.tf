33df46047f1cb761f814dfa60114f4db8fd1e08c
Prayash Thapa <comppro54@gmail.com>
2025-03-18 21:44:46 +0545

    chore:: fix format

--- security_group.tf
+++ security_group.tf
@@ -2,14 +2,14 @@
 resource "aws_security_group" "ecs_sg" {
   name        = "ecs-security-group"
   description = "Allow inbound traffic on port 8000"
-  vpc_id      = "vpc-018a6dba625eb2c82"  # Replace with your VPC ID
+  vpc_id      = "vpc-018a6dba625eb2c82" # Replace with your VPC ID
 
   # Allow inbound traffic on port 8000
   ingress {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow from anywhere (change for security)
+    cidr_blocks = ["0.0.0.0/0"] # Allow from anywhere (change for security)
   }
 
   # Allow all outbound traffic
